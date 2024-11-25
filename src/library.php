569216f1ad135d10b1e0c5c0ba69685886d02a36
alexvvvxela <158572564+alexvvvxela@users.noreply.github.com>
2024-11-25 20:39:13 +0300

    Update library.php

--- src/library.php
+++ src/library.php
@@ -4,14 +4,17 @@ namespace Alexv\Zad9;
 
 use Alexv\Zad9\Book;
 
-class Library {
+class Library
+{
     private $books = [];
 
-    public function addBook(Book $book) {
+    public function addBook(Book $book): void
+    {
         $this->books[] = $book;
     }
 
-    public function removeBookBByTitle($title) {
+    public function removeBookBByTitle($title): bool
+    {
         for ($index = 0; $index < count($this->books); $index++) {
             if ($this->books[$index]->getTitle() === $title) {
                 unset($this->books[$index]);
@@ -22,7 +25,8 @@ class Library {
         return false;
     }
 
-    public function findBooksByAuthor2($author) {
+    public function findBooksByAuthor2($author): array
+    {
         $foundBooks = [];
         for ($i = 0; $i < count($this->books); $i++) {
             if ($this->books[$i]->getAuthor() === $author) {
@@ -32,7 +36,9 @@ class Library {
         return $foundBooks;
     }
 
-    public function listAllBooks() {
+    public function listAllBooks(): array
+    {
         return $this->books;
     }
 }
+
